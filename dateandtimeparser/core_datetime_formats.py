553990bcf2a992469a1c4771e8bd9e4f02d2120b
bharath g s <royalkingpin@gmail.com>
2019-07-09 16:52:04 +0530

    formats

---
+++ dateandtimeparser/core_datetime_formats.py
@@ -0,0 +1,6 @@
+class DateTimeFormats:
+    FORMATS = ['%B %d %y', '%b %d %Y', '%b %d, %Y',
+               '%b %d, %Y', '%B %d, %Y', '%B %d %Y',
+               '%d/%m/%Y', '%d/%m/%y', '%b %Y', '%B%Y',
+               '%b %d,%Y', '%d%b%y', '%d%b', '%b%y',
+               '%d %b %y', '%m/%d', '%m/%d%y']
